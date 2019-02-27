2f6b71d6e295259e52996fbefbd6c74df49f9237
kdcllc <info@kingdavidconsulting.com>
2019-02-27 13:35:02 -0500

    resolve issue #13. Add reference to Bet.AspNetCore library

--- src/Infrastructure/HealthChecks/UriCheck/UriHealthCheckOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Infrastructure.HealthChecks.UriCheck
-{
-    public class UriHealthCheckOptions
-    {
-        public ICollection<UriOptionsSetup> UriOptions { get; } = new List<UriOptionsSetup>();
-    }
-}
--- test/App.unittests/FakeOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App.unittests
-{
-    public class FakeOptions
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
