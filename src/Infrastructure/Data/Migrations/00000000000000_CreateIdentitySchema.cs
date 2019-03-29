e3c3b59b04dec78403b351329fbb8dea16208e15
kdcllc <info@kingdavidconsulting.com>
2019-03-29 17:14:16 -0400

    move to infra

--- src/Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ src/Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace DabarBlog.Data.Migrations
+namespace Infrastructure.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
--- src/Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace DabarBlog.Data.Migrations
+namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
